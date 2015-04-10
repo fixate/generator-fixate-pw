504dd6eabe707842940a94410363b41ded424ac5
Larry Botha <larry@fixate.it>
2015-04-10 02:25:50 +0200

    use execSync to allow interactive shell for bower initialisation

--- app/templates/setup.js
+++ app/templates/setup.js
@@ -1,44 +1,34 @@
-var shell = require("shelljs");
+var shell = require("shelljs/global")
+		execSync = require("execSync");
 
-var Setup = function() {
-	var Setup = function() {
-		this.setupBower();
-		this.setupSecrets();
-		this.setupSymlinks();
+if (!test('-f', 'bower.json')) {
+	if (!which('bower')) {
+		echo('bower not installed');
+		echo('run npm install -g bower && npm install');
+		echo('aborting');
+		exit(1);
+	} else {
+		echo('bower not initialiased... initialising');
+		execSync.run('bower init');
 	}
+} else {
+	echo('bower.json is initialised');
+}
 
-	Setup.prototype.setupBower = function() {
-		if (shell.test('-f', 'bower.json')) {
-			if (!shell.which('bower')) {
-				shell.echo('bower not installed');
-				shell.echo('run npm install -g bower && npm install');
-				shell.echo('aborting');
-				shell.exit(1);
-			} else {
-				shell.echo('bower not initialiased... initialising');
-				shell.exec('bower init');
-			}
-		}
-	}
+if (test('-f', 'secrets.json') && !test('-f', 'secrets.json')) {
+	echo('creating your vey own secrets');
+	cp('secrets-sample.json', 'secrets.json');
+	echo('secrets.json created');
+} else {
+	echo('secrets.json is ready');
+}
 
-	Setup.prototype.setupSecrets = function() {
-		if (test('-f', 'secrets.json') && !test('-f', 'secrets.json')) {
-			shell.echo('creating your vey own secrets');
-			shell.cp('secrets-sample.json', 'secrets.json');
-			shell.echo('secrets.json created');
-		}
-	}
+if (!test('-L', 'styleguide/public/assets')) {
+	echo('symlinking styleguide assets to site assets');
+	cd('styleguide/public');
+	ln('-s', '../../src/site/templates/assets', 'assets');
+	echo('assets symlinked');
+} else {
+	echo('styleguide correctly symlinked to site assets');
+}
 
-	Setup.prototype.setupSymlinks = function() {
-		if (!test('-L', 'styleguide/public/assets')) {
-			shell.exec('symlinking styleguide to assets');
-			shell.cd('styleguide/public');
-			shell.ln('-s', '../../src/site/templates/assets', 'assets');
-			shell.exec('assets symlinked');
-		}
-	}
-
-	return Setup;
-}();
-
-new Setup();
