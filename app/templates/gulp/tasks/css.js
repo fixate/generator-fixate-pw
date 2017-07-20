38224658edb05f46067e58e1c2023b0957165adf
Larry Botha <larry@fixate.it>
2017-07-20 15:48:04 +0200

    update css task with postcss and colorguard

--- app/templates/gulp/tasks/css.js
+++ app/templates/gulp/tasks/css.js
@@ -1,29 +1,29 @@
-const gulp         = require('gulp');
-const autoprefixer = require('gulp-autoprefixer');
-const sass         = require('gulp-sass');
-const sourcemaps   = require('gulp-sourcemaps');
+const gulp = require('gulp');
+const autoprefixer = require('autoprefixer');
+const colorguard = require('colorguard');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
 
 const conf = require('../gulpconfig');
-
-
-
-
 
 //*------------------------------------*\
 //     $CSS
 //*------------------------------------*/
-gulp.task('css', () =>
-  gulp.src([`${conf.path.dev.scss}/**/*.{scss,sass}`])
+gulp.task('css', () => {
+  const postcssPlugins = [
+    autoprefixer({browsers: ['last 2 versions']}),
+    colorguard({allowEquivalentNotation: true}),
+  ];
+
+  return gulp
+    .src([`${conf.path.dev.scss}/**/*.{scss,sass}`])
     .pipe(sourcemaps.init())
     .pipe(sass(conf.sass).on('error', sass.logError))
-    .pipe(autoprefixer({browsers: ['last 2 versions']}))
+    .pipe(postcss(postcssPlugins))
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(conf.path.dev.css))
-);
-
-
-
-
+    .pipe(gulp.dest(conf.path.dev.css));
+});
 
 //*------------------------------------*\
 //     $CSS WATCH
