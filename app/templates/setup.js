2eb774ac5648a2432a572941a4a3e3c35b5e638f
Larry Botha <larry@fixate.it>
2018-02-17 11:17:43 +0200

    remove bower stuff

--- app/templates/setup.js
+++ app/templates/setup.js
@@ -1,19 +1,5 @@
-var shell = require("shelljs/global"),
-    exec = require("child_process").exec;
-
-if (!test('-f', 'bower.json')) {
-  if (!which('bower')) {
-    echo('bower module not installed');
-    echo('run npm install -g bower && npm install');
-    echo('aborting');
-    exit(1);
-  } else {
-    echo('installing bower components');
-    exec('bower install').stdout.pipe(process.stdout);
-  }
-} else {
-  echo('bower.json is initialised');
-}
+var shell = require('shelljs/global'),
+  exec = require('child_process').exec;
 
 if (test('-f', 'gulp/secrets-sample.js') && !test('-f', 'gulp/secrets.js')) {
   echo('creating your very own secrets');
