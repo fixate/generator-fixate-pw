2321784ba4fa8bdd4ae9ed4ecdaeb0ad9731aabb
Larry Botha <larry@fixate.it>
2020-01-09 16:20:11 -0500

    fix utils for Gulp 4

--- app/templates/gulp/tasks/utils/index.js
+++ app/templates/gulp/tasks/utils/index.js
@@ -1,18 +1,64 @@
-const exec = require('child_process').exec;
+const {exec, spawn} = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
-const execCommand = (cmd, done) =>
-  exec(cmd, function(err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    if (err) {
-      console.log(err);
+const execCommand = (cmd, done, cb) => {
+  const childProcess = exec(cmd);
+
+  childProcess.stdout.on('data', data => {
+    const msg = typeof data === 'string' ? data.trim() : data;
+    console.log(`stdout:`, msg);
+  });
+
+  childProcess.stderr.on('data', data => {
+    const msg = typeof data === 'string' ? data.trim() : data;
+    console.log(`stderr:`, msg);
+  });
+
+  childProcess.on('exit', code => {
+    if (typeof cb === 'function') {
+      cb(code);
     }
-    if (done && typeof done === 'function') {
+
+    if (typeof done === 'function') {
       return done();
+    } else {
+      throw new Error(
+        `execCommand requires Gulp's 'done' callback to signal the end of the stream`,
+      );
     }
   });
+
+  return childProcess;
+};
+
+const spawnCommand = (cmd, args, done, cb) => {
+  const childProcess = spawn(cmd, args);
+
+  childProcess.stdout.on('data', data => {
+    console.log(`stdout:`, data);
+  });
+
+  childProcess.stderr.on('data', (...args) => {
+    console.log(`stderr:`, ...args);
+  });
+
+  childProcess.on('exit', code => {
+    if (typeof cb === 'function') {
+      cb(code);
+    }
+
+    if (typeof done === 'function') {
+      return done();
+    } else {
+      throw new Error(
+        `spawnCommand requires Gulp's 'done' callback to signal the end of the stream`,
+      );
+    }
+  });
+
+  return childProcess;
+};
 
 const getNewestFile = function(dir) {
   const files = fs.readdirSync(dir);
@@ -33,5 +79,6 @@ const getNewestFile = function(dir) {
 
 module.exports = {
   execCommand,
+  spawnCommand,
   getNewestFile,
 };
